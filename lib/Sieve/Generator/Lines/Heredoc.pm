7e76f2b94ec41db7fd340ebfda1208624dbdaf20
Ricardo Signes <rjbs@semiotic.systems>
2024-11-23 21:54:10 -0500

    Heredoc: indent the start as needed

--- lib/Sieve/Generator/Lines/Heredoc.pm
+++ lib/Sieve/Generator/Lines/Heredoc.pm
@@ -7,7 +7,8 @@ with 'Sieve::Generator::Lines';
 has text => (is => 'ro', required => 1);
 
 sub as_sieve ($self, $i = undef) {
-  my $str = "text:\n" . $self->text;
+  my $indent = q{  } x ($i // 0);
+  my $str = "${indent}text:\n" . $self->text;
   $str .= "\n" unless $str =~ /\n\z/;
   $str =~ s/^\./../mg;
   return "$str.\n";
